f0adfc722f281135bba5e6b71a17d90406eafbf3
Don Mosites <don.mosites@fluidity.io>
2021-10-12 15:30:20 +0200

    Adding example .env file

---
+++ api/utils.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.decimals = void 0;
+exports.decimals = {
+    '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea': 18,
+    '0xc778417e063141139fce010982780140aa0cd5ab': 18,
+};
