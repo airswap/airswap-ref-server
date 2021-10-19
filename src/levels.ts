979f5b0efe6799b989830a3e14693255c79b2f2f
Don Mosites <don.mosites@fluidity.io>
2021-10-19 13:26:39 +0200

    Adjust levels

--- src/levels.ts
+++ src/levels.ts
@@ -8,8 +8,8 @@ const levels = [
   {
     baseToken: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
     quoteToken: '0xc778417e063141139fce010982780140aa0cd5ab',
-    bid: [['1000', '1.1']],
-    ask: [['1000', '0.9']]
+    bid: [['1000', '1']],
+    ask: [['1000', '1']]
   },
 ]
 
