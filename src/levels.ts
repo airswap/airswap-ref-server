a9ed677721074f9ca2fcbd860b4e78a44462916f
Don Mosites <don.mosites@fluidity.io>
2021-11-11 10:38:05 +0100

    Add minimums to pricing

--- src/levels.ts
+++ src/levels.ts
@@ -2,12 +2,14 @@ export const LLLevels = [
   {
     baseToken: '0xc778417e063141139fce010982780140aa0cd5ab',
     quoteToken: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
+    minimum: '5000000000000000',
     bid: [['1000', '1']],
     ask: [['1000', '1']]
   },
   {
     baseToken: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
     quoteToken: '0xc778417e063141139fce010982780140aa0cd5ab',
+    minimum: '5000000000000000',
     bid: [['1000', '0.9']],
     ask: [['1000', '1.1']]
   },
@@ -17,12 +19,14 @@ export const RFQLevels = [
   {
     baseToken: '0xc778417e063141139fce010982780140aa0cd5ab',
     quoteToken: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
+    minimum: '5000000000000000',
     bid: [['1000', '0.9']],
     ask: [['1000', '1.1']]
   },
   {
     baseToken: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
     quoteToken: '0xc778417e063141139fce010982780140aa0cd5ab',
+    minimum: '5000000000000000',
     bid: [['1000', '1']],
     ask: [['1000', '1']]
   },
